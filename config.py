a9ff47b594ec019cd2412f773ee024bb9a26b51e
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2026-01-06 20:20:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,27 +12,27 @@ id_pattern = re.compile(r'^.\d+$')
 # --
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
+    API_ID    = os.environ.get("API_ID", "24607450")
+    API_HASH  = os.environ.get("API_HASH", "204768fe292a431ee267aebaa1dbbd11")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    PORT = os.environ.get("PORT", "8980")
+    PORT = os.environ.get("PORT", "8080")
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME", "RexBots")     
+    DB_NAME = os.environ.get("DB_NAME", "test")     
     DB_URL  = os.environ.get("DB_URL", "")
  
     # other configs
-    ADMIN_URL = "https://t.me/adityaabhinav"
-    DUMP_CHANNEL = os.environ.get("DUMP_CHANNEL", "-1002257657458")
+    ADMIN_URL = "https://t.me/Explainer_AYU"
+    DUMP_CHANNEL = os.environ.get("DUMP_CHANNEL", "-1002560282913")
     DUMP = True
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://ibb.co/Cs0zsrCn")
     LEADERBOARD_PIC = os.environ.get("LEADERBOARD_PIC", "https://ibb.co/RGx4RmDg")
-    OWNER_ID = int(os.environ.get("OWNER_ID", "6701907262"))
-    SUPPORT_CHAT = int(os.environ.get("SUPPORT_CHAT", "-1002871570975"))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-    FSUB_PIC = os.environ.get("FSUB_PIC", "")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1685470205"))
+    SUPPORT_CHAT = int(os.environ.get("SUPPORT_CHAT", "-1002302874208"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002560282913"))
+    FSUB_PIC = os.environ.get("FSUB_PIC", "https://envs.sh/og0.jpg")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "autorenam_bot")
     LEADERBOARD_DELETE_TIMER = 30
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
